a1ecd9dbdcd9f357b38a2ddbfc3fa1c9d8ee5d10
iFTAKHER_HRiDOY <109151901+iftakher-hridoy@users.noreply.github.com>
2023-06-09 14:40:19 +0600

    Airline Ticket Reservation Management System
    * Flight search option (Round-trip, one-way, etc)
    * Flight reservations,
    * Seat upgrades,
    * Flight cancellation,
    * Flight quotation booking system
    * Other travel services (hotel, transfer, etc.)

---
+++ airline-ticket-reservation/user_reg_success.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+		<title>
+			Create New User Account
+
+			<div class='love'>
+        <p> Desgin With <img src="images/love_copy.png"/> BY <a href='https://dpi.gov.bd/' target='_blank'> D.P.i PROJECT 2.0 </a></p>
+    </div>
+			
+		</title>
+		<style>
+			input {
+    			border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 7px 30px;
+			}
+			input[type=submit] {
+				background-color: #030337;
+				color: white;
+    			border-radius: 4px;
+    			padding: 7px 45px;
+    			margin: 0px 135px
+			}
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<img class="logo" src="images/shutterstock_22.jpg"/> 
+		<h1 id="title">
+			D.P.I AIRLINES
+		</h1>
+		<div>
+			<ul>
+				<li><a href="home_page.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+				<li><a href="login_page.php"><i class="fa fa-ticket" aria-hidden="true"></i> Book Tickets</a></li>
+				<li><a href="home_page.php"><i class="fa fa-plane" aria-hidden="true"></i> About Us</a></li>
+				<li><a href="home_page.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact Us</a></li>
+				<li><a href="login_page.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
+			</ul>
+		</div>
+		<br>
+		<h3>Thank You For Project 2.0 & New user successfully registered! Sir Please Login into your account to book tickets.</h3>
+	</body>
+
+
+	<div class='love'>
+        <p> Desgin With <img src="images/love_copy.png"/> BY <a href='https://dpi.gov.bd/' target='_blank'> D.P.i PROJECT 2.0 </a></p>
+    </div>
+
+
+</html>
